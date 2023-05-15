3c66c4907d4e4323a7de4df93541f9f082dac8eb
PatchOfScotland <d.marchant@ed-alumni.net>
2023-05-15 10:42:22 +0200

    added text string to send email function

--- functionality/notifications.py
+++ functionality/notifications.py
@@ -2,6 +2,10 @@
 import smtplib
 
 def send_email(host:str, sender:str, reciever:str, message:str,)->None:
+    """Sends an email according to the given parameters. Note that this is a 
+    very barebones proof-of-concept implementation and does not currently 
+    provide any of the necessary certificates etc for this to work out of box 
+    unless a user has manually set them up beforehand."""
     try:         
         port:int=smtplib.SMTP_PORT
         if ':' in host:
